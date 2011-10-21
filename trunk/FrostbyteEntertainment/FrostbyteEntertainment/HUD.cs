8bc55ab474a1fec87a961be7070bbd6d243e9e1e
djnemec <djnemec@5a4e93e6-1482-3aab-bfea-f94364c5357d>
2011-10-21 16:11:19 +0000

    Created Progress Bar

---
+++ trunk/FrostbyteEntertainment/FrostbyteEntertainment/HUD.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Frostbyte
+{
+    interface HUDTheme
+    {
+        Color TextColor { get; }
+
+    }
+
+    internal class GenericTheme : HUDTheme
+    {
+        public Color TextColor { get { return Color.White; } }
+    }
+
+    internal class EarthTheme : HUDTheme
+    {
+        public Color TextColor { get { return Color.BurlyWood; } }
+    }
+
+    internal class HUD
+    {
+        #region Constructors
+        internal HUD()
+            : this(new GenericTheme())
+        {
+        }
+
+        internal HUD(HUDTheme theme)
+        {
+
+        }
+        #endregion
+
+        #region Variables
+        private int mHealth;
+        internal int Health { 
+            get { return mHealth; }
+            set { mHealth = value; }
+        }
+        #endregion
+    }
+}
