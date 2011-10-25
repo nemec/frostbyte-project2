0b1bb347ffb3d9a52880e847466a4c01e23309d0
iturtleman128@gmail.com <iturtleman128@gmail.com@5a4e93e6-1482-3aab-bfea-f94364c5357d>
2011-10-25 02:09:52 +0000

    Added all golem anims

--- FrostbyteEntertainment/FrostbyteEntertainment/Enemies/Golem.cs
+++ FrostbyteEntertainment/FrostbyteEntertainment/Enemies/Golem.cs
@@ -20,6 +20,16 @@ namespace Frostbyte.Enemies
             This.Game.CurrentLevel.GetAnimation("golem-idle-right.anim"),
             This.Game.CurrentLevel.GetAnimation("golem-idle-diagup.anim"),
             This.Game.CurrentLevel.GetAnimation("golem-idle-up.anim"),
+            This.Game.CurrentLevel.GetAnimation("golem-walk-down.anim"),
+            This.Game.CurrentLevel.GetAnimation("golem-walk-diagdown.anim"),
+            This.Game.CurrentLevel.GetAnimation("golem-walk-right.anim"),
+            This.Game.CurrentLevel.GetAnimation("golem-walk-diagup.anim"),
+            This.Game.CurrentLevel.GetAnimation("golem-walk-up.anim"),
+            This.Game.CurrentLevel.GetAnimation("golem-attack-down.anim"),
+            This.Game.CurrentLevel.GetAnimation("golem-attack-diagdown.anim"),
+            This.Game.CurrentLevel.GetAnimation("golem-attack-right.anim"),
+            This.Game.CurrentLevel.GetAnimation("golem-attack-diagup.anim"),
+            This.Game.CurrentLevel.GetAnimation("golem-attack-up.anim"),
         };
 
         #endregion Variables
