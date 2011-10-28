b63ac11f62115ed10dd126ec8bfd87797e51e6a6
bruce@thebevilacquas.com <bruce@thebevilacquas.com@5a4e93e6-1482-3aab-bfea-f94364c5357d>
2011-10-28 15:36:49 +0000

    Added golem attacks

--- trunk/FrostbyteEntertainment/FrostbyteEntertainment/Personality.cs
+++ trunk/FrostbyteEntertainment/FrostbyteEntertainment/Personality.cs
@@ -142,6 +142,8 @@ namespace Frostbyte
                     yield return null;
                 }
 
+                master.isAttackingAllowed = true;
+
                 yield return null;
             }
         }
