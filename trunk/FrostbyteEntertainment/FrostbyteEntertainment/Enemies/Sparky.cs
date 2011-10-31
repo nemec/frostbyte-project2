19c1749a0dc2a764a0c5e7b76035a22ea35d6176
vincentgarcia00@gmail.com <vincentgarcia00@gmail.com@5a4e93e6-1482-3aab-bfea-f94364c5357d>
2011-10-31 22:25:56 +0000

    Fixed Wandering Minstrel Personality, began work on Electric Bat

--- trunk/FrostbyteEntertainment/FrostbyteEntertainment/Enemies/Sparky.cs
+++ trunk/FrostbyteEntertainment/FrostbyteEntertainment/Enemies/Sparky.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
+using Microsoft.Xna.Framework;
 
 namespace Frostbyte.Enemies
 {
@@ -12,27 +13,35 @@ namespace Frostbyte.Enemies
     internal partial class Sparky : Frostbyte.Enemy
     {
         #region Variables
+        bool changeState = false;
         static List<Animation> Animations = new List<Animation>(){
             This.Game.CurrentLevel.GetAnimation("antibody.anim")
         };
 
         #endregion Variables
 
-        internal Sparky(string name, float speed, int health)
+        internal Sparky(string name, float speed, int health, Vector2 initialPos)
             : base(name, new Actor(Animations), speed, health)
         {
+            Pos = initialPos;
             movementStartTime = new TimeSpan(0, 0, 1);
             ElementType = Element.Lightning;
+            Personality = new WanderingMinstrelPersonality(this);
         }
 
         protected override void updateMovement()
         {
-            throw new NotImplementedException();
+            if (changeState)
+            {
+                movementStartTime = TimeSpan.MaxValue;
+            }
+            List<Sprite> targets = This.Game.CurrentLevel.GetSpritesByType("Mage");
+            Personality.Update();
         }
 
         protected override void updateAttack()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
     }
