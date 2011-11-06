df856bcc77011e6c9717b362f479bd1338c1549c
iturtleman128@gmail.com <iturtleman128@gmail.com@5a4e93e6-1482-3aab-bfea-f94364c5357d>
2011-11-06 21:23:37 +0000

    changed editor to create more compact spritesheets when with and height aren't similar. Also added tool for batch image processing

---
+++ trunk/Tools/batch image editor/resize[335x440+10+5].cmd
@@ -0,0 +1,10 @@
+cd All-DiagDown
+convert *.png[335x440+10+5] cropped%03d.png
+cd ..\All-DiagUp
+convert *.png[335x440+10+5] cropped%03d.png
+cd ..\All-Down
+convert *.png[335x440+10+5] cropped%03d.png
+cd ..\All-Right
+convert *.png[335x440+10+5] cropped%03d.png
+cd ..\All-Up
+convert *.png[335x440+10+5] cropped%03d.png
