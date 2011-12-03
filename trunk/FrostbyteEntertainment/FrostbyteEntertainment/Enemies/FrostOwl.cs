444f020222d7f030225ea26b4b3c95af1b008673
bruce@thebevilacquas.com <bruce@thebevilacquas.com@5a4e93e6-1482-3aab-bfea-f94364c5357d>
2011-12-03 05:31:37 +0000

    Frost Owls now behave like electric bats

--- trunk/FrostbyteEntertainment/FrostbyteEntertainment/Enemies/FrostOwl.cs
+++ trunk/FrostbyteEntertainment/FrostbyteEntertainment/Enemies/FrostOwl.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
 
 namespace Frostbyte.Enemies
 {
@@ -35,17 +37,62 @@ namespace Frostbyte.Enemies
         {
             SpawnPoint = initialPosition;
             movementStartTime = new TimeSpan(0, 0, 1);
+            Personality = new PseudoWanderPersonality(this);
             ElementType = Element.Water;
         }
 
         protected override void updateMovement()
         {
-
+            Personality.Update();
         }
 
         protected override void updateAttack()
         {
+            if (This.gameTime.TotalGameTime >= attackStartTime + new TimeSpan(0, 0, 2) && isAttackAnimDone)
+            {
+                float range = 450.0f;
+                List<Sprite> targets = (This.Game.CurrentLevel as FrostbyteLevel).allies;
+                Sprite target = GetClosestTarget(targets, range);
+                if (target != null)
+                {
+                    attackStartTime = This.gameTime.TotalGameTime;
 
+                    int attackRange = 12;
+
+                    //Create Particle Emmiter
+                    Effect particleEffect = This.Game.CurrentLevel.GetEffect("ParticleSystem");
+                    Texture2D lightning = This.Game.CurrentLevel.GetTexture("sparkball");
+                    ParticleEmitter particleEmitterLightning = new ParticleEmitter(1000, particleEffect, lightning);
+                    particleEmitterLightning.effectTechnique = "NoSpecialEffect";
+                    particleEmitterLightning.blendState = BlendState.Additive;
+                    (particleEmitterLightning.collisionObjects.First() as Collision_BoundingCircle).Radius = attackRange;
+                    (particleEmitterLightning.collisionObjects.First() as Collision_BoundingCircle).createDrawPoints();
+                    particleEmitters.Add(particleEmitterLightning);
+
+                    mAttacks.Add(Attacks.T1Projectile(target,
+                                              this,
+                                              5,
+                                              10,
+                                              new TimeSpan(0, 0, 0, 1, 750),
+                                              attackRange,
+                                              6f,
+                                              false,
+                                              delegate(OurSprite attacker, Vector2 direction, float projectileSpeed, ParticleEmitter particleEmitter)
+                                              {
+                                                  Vector2 tangent = new Vector2(-direction.Y, direction.X);
+                                                  for (int i = -5; i < 6; i++)
+                                                  {
+                                                      particleEmitter.createParticles(-direction * projectileSpeed * 5,
+                                                                                                  tangent * -i * 40,
+                                                                                                  particleEmitter.GroundPos + tangent * i * ParticleEmitter.EllipsePerspectiveModifier - direction * (Math.Abs(i) * 7),
+                                                                                                  4,
+                                                                                                  300);
+                                                  }
+                                              },
+                                              particleEmitterLightning,
+                                              Vector2.Zero).GetEnumerator());
+                }
+            }
         }
     }
 }
