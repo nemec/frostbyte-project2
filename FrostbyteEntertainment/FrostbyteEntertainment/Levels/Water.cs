7705adbdebe2ced8d2572b903f4b8806a906855f
bruce@thebevilacquas.com <bruce@thebevilacquas.com@5a4e93e6-1482-3aab-bfea-f94364c5357d>
2011-12-06 04:15:33 +0000

    Moved water level boss player spawn point Trigger is 32 px lower

--- FrostbyteEntertainment/FrostbyteEntertainment/Levels/Water.cs
+++ FrostbyteEntertainment/FrostbyteEntertainment/Levels/Water.cs
@@ -24,20 +24,17 @@ namespace Frostbyte.Levels
 
             Characters.Mage mage = new Characters.Mage("Player 1", PlayerIndex.One, new Color(255, 0, 0));
             mage.SpawnPoint = new Microsoft.Xna.Framework.Vector2(6 * Tile.TileSize, 52 * Tile.TileSize);
-            mage.SpawnPoint = new Vector2(10143, 3771);
+            mage.SpawnPoint = new Vector2(147*64+32, 68*64+32);
             mage.Speed = 1;
             mage.Scale = 0.7f;
             l.HUD.AddPlayer(mage);
 
             Characters.Mage mage2 = new Characters.Mage("Player 2", PlayerIndex.Two, new Color(114, 255, 255));
             mage2.SpawnPoint = new Microsoft.Xna.Framework.Vector2(6 * Tile.TileSize, 53 * Tile.TileSize);
-            mage2.SpawnPoint = new Vector2(10143, 3771);
+            mage2.SpawnPoint = new Vector2(147*64+32, 68*64+32);
             mage2.Speed = 1;
             mage2.Scale = 0.7f;
             l.HUD.AddPlayer(mage2);
-
-            Enemies.WaterBlobBoss b = new Enemies.WaterBlobBoss("boss", new Vector2(10143, 4171));
-            b.Speed = 1;
             
             This.Game.AudioManager.AddBackgroundMusic("Music/WaterBG");
             This.Game.AudioManager.PlayBackgroundMusic("Music/WaterBG", 0.3f);
