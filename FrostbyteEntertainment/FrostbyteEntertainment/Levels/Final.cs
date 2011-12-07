5f3702c06b8388a525c192198ada1ec236964dcd
bruce@thebevilacquas.com <bruce@thebevilacquas.com@5a4e93e6-1482-3aab-bfea-f94364c5357d>
2011-12-07 08:59:42 +0000

    Added collision circles for waterboss charge Removed fadetext for FireLevel-BossRoom Manually added spikes for FireLevel-BossRoom

--- FrostbyteEntertainment/FrostbyteEntertainment/Levels/Final.cs
+++ FrostbyteEntertainment/FrostbyteEntertainment/Levels/Final.cs
@@ -24,14 +24,14 @@ namespace Frostbyte.Levels
 
             Characters.Mage mage = new Characters.Mage("Player 1", PlayerIndex.One, new Color(255, 0, 0));
             mage.SpawnPoint = new Microsoft.Xna.Framework.Vector2(65 * Tile.TileSize, 37 * Tile.TileSize);
-            mage.SpawnPoint = new Vector2(1855, 2640);
+            //mage.SpawnPoint = new Vector2(1855, 2640);
             mage.Speed = 1;
             mage.Scale = 0.7f;
             l.HUD.AddPlayer(mage);
 
             Characters.Mage mage2 = new Characters.Mage("Player 2", PlayerIndex.Two, new Color(114, 255, 255));
             mage2.SpawnPoint = new Microsoft.Xna.Framework.Vector2(66 * Tile.TileSize, 37 * Tile.TileSize);
-            mage2.SpawnPoint = new Vector2(1875, 2640);
+            //mage2.SpawnPoint = new Vector2(1875, 2640);
             mage2.Speed = 1;
             mage2.Scale = 0.7f;
             l.HUD.AddPlayer(mage2);
